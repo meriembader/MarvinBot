8facb056128b2a1f3da6a6cea97eec2b326b8b76
sghirlouay <meriembader8@gmail.com>
2021-04-05 10:20:34 +0100

    done

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -4,8 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
+const data = require('../mylogreg.json');
 
 
+const trainingData = data.map(item => ({
+  output: item.X_train
+}));
+network.train(trainingData, {
+  iterations: 2000
+});
+/*
+app.get('/predict',function(req,res){
+	console.log(req.query);
+res.render('index');
+})*/
 /*********************************Chat************ */
 
 const Token= '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCSIweI7uFzB07K\nRlH+Wo5bqiT2Tx/Jql18JxWJ7+pOWfbLU2mA8aFqOzyOZjmt1Mt0OJnaDgQPEJCU\nNcB0D4AozvSPXgNdUrAJzHTef/mUCEw319B4gjs98uRCncSrMuvIWUKf+K1uEmU1\n9SHQLiLyTty3EVvrEeE0A7ewioP+lrXKzGmF7UCImExRVaUL7w8mFQCt5l80r3ha\neknZ35YWP+O4fv4ZtOEs0HEvgK5j/U76YUJWUTGHChTG1+4opeZm7PB5ASWfC3y/\noov+++c90Ti2BehFr6iS+F55ApPcLyezWIx7oO3lyOY8as5iBOYfh/pAY3ozxj0R\nmLEOCgdvAgMBAAECggEAAh0yCuxu5BF2o2ZS4cKXv53dkXLmS1LE0nGbuqiboVYb\nsagEnA/ZppPWqBGQFZ4KO7fSwWFuqyuxiE/rnAbH8zPAYT0jbglBHkIG/6S5A361\nzmncMPoZ5N4HBAbVid3opJNB/daXCx6rJgNSKtThLGYMX8cHS70w+gqVJ59vEhv3\nbwqx0469AqWonbD0BhgrML9oMm8OFaIIImszAxMKCxkHg7HjcYx1a6qos5ZVKAMw\nMeeHtRtPsUy0MN0Q6Z5qaYGS8t5tCQuZkh4p3+ffP+ZScUegv8JLDfMVNCXFkNAo\nfrd5Hi5MKSfBkzeeU+cFwmm+027LYy757dX/D0MQuQKBgQDFp7MiiupOs2/KQttA\ng7y6DRmnARthl6SAaCExQeeTONWVMCoeYrIDNe17pXFltlnrJ/nAvP39qcec88tH\n7SbKmUUG/JkXRufCsZQ1CXbgILNywBo2bDFqpPtevD5vzB5BK6MEeGSn8NwbIS11\nynMePZCnZQ6nVeLDxSokLQAJGQKBgQC9RjmKufoD/l46rSLFOb3qJyalV+ZCCFNq\nn3UokRa0O3Ws3Sn1/UgnvjL1tEokCSP19LG2d4xFHaFEgIblCirfaq1m3mGQFAnK\n6TsvRDP1wkre98+kqxiIWf+ehnDIE/9/UbGuED0WaDVKN3tXrg9uqGlc1ojtuHgv\n2HBkOpg9xwKBgQCzWGl4rGfSpYqIEoXzUhXLE9v051I7CWfgg5uqoExKD8XtKtjs\n3Uz9EM+94+6zBfUtJQa/1jz5EDbpuWT4jL0oFE7H+ifHMTVWOd9rCH1u/P0W4Cxo\nuV9VjwHvggt9Uc8Z/0hZ5AwhPrHZhA9F1D8Eb4Rz/4R+XbX3QU3tqU9g8QKBgAhA\nTHfgvaZ461CxhYeFieLwS/3/Q/ly9Xf7dZcMJCo9QVcE2TRxEzkwUHZnoqfJgjjR\nrwGeWo4UyZf75mWLuOy+cMrAPQ6T4Q1od30LAxzemHb55KJwFwSyr7MQd1AxlvzM\n6esFqDQW3OPcYVMkMmYHj3hNnfCHDy4+yD+GUma3AoGABOBqmPoVoPUvp4tJB7z5\nlbRlp3/q39yt9C+GYkBLeRH6vK8MqefhpxzmsieF0dY9nSZgSXOj21VwLRUdqr0a\nFMdN6u8WVw0hLxvFsXjaBlSaFJ+dBsvE00+ANt06yeu8cOtg5zeML0cn2m1gQ0xB\nOsEAXTOZaNFOHzCI/sqqZgY=\n-----END PRIVATE KEY-----\n'
