31d7278f9100504b25449d0bfdfc32f784df47e6
sghirlouay <meriembader8@gmail.com>
2021-04-26 10:15:08 +0100

    done

--- NodeServer/routes/user.routes.js
+++ NodeServer/routes/user.routes.js
@@ -11,7 +11,7 @@ var app = express();
 const _ =require('lodash');
 const controller = require("../controllers/user.controller");
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey("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");
+sgMail.setApiKey("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");
 const { validationResult } = require('express-validator');
 const changepassword = require ('../models/ChangePassword');
 const  check  = require('check');
@@ -22,6 +22,8 @@ var io = require('socket.io')(server);
 /********************************************** */
 const { validator} = require('validator') ;
 const sendEmail = require( '../utils/SendMail');
+const { stat } = require('fs');
+
 
 const User = db.user;
 const Role = db.role;
@@ -256,37 +258,158 @@ const userUpd = await user.findByIdAndUpdate(req.params.id);
 
 
 router.post('/forgotpassword', async(req, res, next) => {
-  try {
-    const { email } = req.body;
-    //const User = db.user;
-    const errors = validationResult(req);
-    const test = await user.findOne({ where: { email } });
-    if (!email) {
-      return res.status(400).send({ error: 'Email is required' });
-    }
-    if (!errors.isEmpty()) { //ok
-      return res.status(400).send({ error: 'emplttyyyy' });
-    }
-    if (!user) {
-      return res.status(404).send({ error: 'User not found' });
-    }
-   
-    var token = jwt.sign({ id: user.id }, "0123456789", {
-      expiresIn: 31536000  // 24 hours
+  const { email } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // Getting the value of the first error
+    const firstError = errors.array().map((error) => error.msg)[0];
+    // réponse HTTP 422 : serveur a compris le type de contenu et que la syntaxe de la requête est correcte mais le serveur n'a pas été en mesure de réaliser les instructions demandées.
+    return res.status(422).json({
+      errors: firstError,
     });
-    const link = `${req.protocol}://localhost:3001/reset_password/${token}`;
-    await sendEmail(
-      email,
-      'meriem.bader1@esprit.tn',
-      'Best To Do password reset',
-      `
-      <div>Click the link below to reset your password</div><br/>
-      <div>${link}</div>
-      `
+  } else {
+    //find if the mail passed by user is his own mail (stored in our DB).
+    user.findOne(
+      {
+        email,
+      },
+      (err, user) => {
+        //if error or the mail is not the same.
+        if (err || !user) {
+          return res.status(400).json({
+            error:
+              "User with that email does not exist. Enter your email address used in this application",
+          });
+        }
+
+        // If exist generate a token.
+        const token = jwt.sign(
+          {
+            //token with this ID
+            _id: user._id,
+          },
+          process.env.RESET_PASSWORD_KEY,
+          {
+            expiresIn: "10m", //token expires in 10 minutes
+          }
+        );
+        // Send email with this token
+        const emailData = {
+          from: process.env.EMAIL_FROM,
+          to: email,
+          subject: `Elegance App - Password Reset link`,
+          html: `
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+          <tr>
+              <td bgcolor="#02174C" align="center" style="padding: 0px 10px 0px 10px;">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                          <td bgcolor="#02174C" align="center" valign="top" style="padding: 10px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: white; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                              <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Welcome AGAIN!</h1> <img src=" http://cdn.mcauto-images-production.sendgrid.net/a9fd540df47a93d6/c7bc4c1e-fe84-45b2-a4bc-609c78775478/332x127.png" width="230" height="100" style="display: block; border: 0px;" />
+                          </td>
+                      </tr>
+                  </table>
+              </td>
+          </tr>
+          <tr>
+              <td bgcolor="#BF1922" align="center" style="padding: 0px 10px 0px 10px;">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                              <p style="margin: 0;">Trouble signing in? <br><br>
+                            Resetting your password is easy. <br><br> Just press the button below and follow the instructions. We’ll have you up and running in no time. </p>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td bgcolor="#ffffff" align="left">
+                              <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                  <tr>
+                                      <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
+                                          <table border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                  <td align="center" style="border-radius: 3px;" bgcolor="#BF1922"><a href=${process.env.CLIENT_URL}reset/${token} target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px;  display: inline-block;">Reset Your Password</a></td>
+                                              </tr>
+                                          </table>
+                                      </td>
+                                  </tr>
+                              </table>
+                          </td>
+                      </tr> <!-- COPY -->
+                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; text-align: 'center'; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                            <p style="margin: 0;">This link will expire after <b>10 minutes.</b></p>
+                          </td>
+                      </tr> <!-- COPY -->
+                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                              <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+                          </td>
+                      </tr> <!-- COPY -->
+                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                              <p style="margin: 0;"><a href="#" target="_blank" style="color: #BF1922;">${process.env.CLIENT_URL}reset/${token}</a></p>
+                          </td>
+                      </tr>
+                                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                              <p style="margin: 0;">This email may contain sensetive information. <br><br> If you did not make this request then please ignore this email.<br><br></p>
+                          </td>
+                      </tr> <!-- COPY -->
+                      <tr>
+                          <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                              <p style="margin: 0;">Cheers,<br>Rebirth Team</p>
+                          </td>
+                      </tr>
+                  </table>
+              </td>
+          </tr>
+          
+          <tr>
+              <td bgcolor="#BF1922" align="center" style="padding: 0px 10px 0px 10px;">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                          <td bgcolor="#BF1922" align="left" style="padding: 0px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;"> <br>
+                          </td>
+                      </tr>
+                  </table>
+              </td>
+          </tr>
+          </table>    
+                `,
+        };
+
+        return user.updateOne(
+          {
+            resetPasswordLink: token,
+          },
+          (err, success) => {
+            if (err) {
+              console.log("RESET PASSWORD LINK ERROR", err);
+              return res.status(400).json({
+                error:
+                  "Database connection error on user password forgot request",
+              });
+            } else {
+              sgMail
+                .send(emailData)
+                .then((sent) => {
+                  // console.log('SIGNUP EMAIL SENT', sent)
+                  return res.json({
+                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+                  });
+                })
+                .catch((err) => {
+                  console.log("SIGNUP EMAIL SENT ERROR", err);
+                  return res.json({
+                    message: err.message,
+                  });
+                });
+            }
+          }
+        );
+      }
     );
-  return res.status(200).send({ message: 'Password reset link has been successfully sent to your inbox' });
-  } catch (e) {
-    return next(new Error(e));
   }
 }
 )
@@ -369,6 +492,8 @@ router.get('/count',(req,res)=>{
 
 
 })
+
+
 /*router.put('/user-profile/:id', async(req, res, next) => {
   const url = req.protocol + '://' + req.get('host')
     console.log(url);
@@ -399,8 +524,25 @@ router.get('/count',(req,res)=>{
  
 })
 */
+/*
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb://localhost:27017/";
+const DATABASE_NAME = "MarvinBot";
+var database, collection;
+MongoClient.connect(CONNECTION_URL, 
+  { useNewUrlParser: true }, (error, client)=> {
+    if(error) {
+        throw error;
+    }
+    database = client.db(DATABASE_NAME);
+    collection = database.collection("users");
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+});
 
-
-
-
+app.get("/stat", (request, response) => {
+  collection.agregate([
+    { "$group":{"_id":"$role","count": {"$sum": 1}}}
+  ])
+});*/
 module.exports = router;
